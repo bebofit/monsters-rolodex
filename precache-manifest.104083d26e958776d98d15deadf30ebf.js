b6058e074a7bc236767ec95ba3efa16614527be2
Abdelrahman Soliman <bebofit@aucegypt.edu>
2020-02-10 18:50:48 +0200

    Updates

---
+++ precache-manifest.104083d26e958776d98d15deadf30ebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a914bd19544cd77495644e42d03dade1",
+    "url": "/bebofit/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "990a38b828c89a980c33",
+    "url": "/bebofit/monsters-rolodex.git/static/css/main.7eb8f5b6.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/bebofit/monsters-rolodex.git/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bebofit/monsters-rolodex.git/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990a38b828c89a980c33",
+    "url": "/bebofit/monsters-rolodex.git/static/js/main.ba6efda6.chunk.js"
+  },
+  {
+    "revision": "a857ee21446534a3a784",
+    "url": "/bebofit/monsters-rolodex.git/static/js/runtime-main.38e4b3df.js"
+  }
+]);
